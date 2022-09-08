0e7c9877e6b15f589a006e3d9a67e20f9279d6fd
Liisagrant <lisagrant1990@gmail.com>
2022-09-08 13:58:54 +0200

    fixed some things and added some tasks for today

--- debugging/js/script.js
+++ debugging/js/script.js
@@ -1,114 +1,172 @@
-// //1
+console.log();
+// 2.
+console.clear();
 
-// console.log("hello");
+console.warn();
+console.error();
+console.table();
 
-// const name = "lisa";
-// console.log(name);
+// 1
 
-// const responseDataOne = [
-//   {
-//     name: "lisa",
-//     age: "32",
-//   },
-//   {
-//     name: "Mia",
-//     age: "40",
-//   },
-// ];
+console.log("Hello i am here");
 
-// const responseDataTwo = [
-//   {
-//     name: "lisa",
-//     age: "32",
-//   },
-//   {
-//     name: "Mia",
-//     age: "40",
-//   },
-// ];
+const name = "Hesh";
 
-// console.log("nr One:", responseDataOne);
-// console.log("nr Two:", responseDataTwo);
+console.log(name);
 
-// console.log(1 + 2);
-// const sum = 1 + 2;
-// console.log("hei " + "på " + sum + " deg");
-// console.log(`hei på deg ${sum}`);
+const responseDataOne = [
+  {
+    name: "hesham",
+    age: "30",
+  },
+  {
+    name: "Emil",
+    age: "29",
+  },
+];
 
-// // 2
-// // console.clear();
+const responseDataTwo = [
+  {
+    name: "hesham",
+    age: "30",
+  },
+  {
+    name: "Emil",
+    age: "29",
+  },
+];
 
-// // 3
-// console.table(responseDataOne);
-// console.log(responseDataOne);
-// //så kan man sammenligne
+console.log("responseDataOne: ", responseDataOne);
+console.log("responseDataTwo: ", responseDataTwo);
 
-// 4
-// console.time(); //starts timer
-// console.end(); //end timer
+console.log(1 + 2);
 
-// function counter() {
-//   for (let i = 0; i <= 1000; i++) {
-//     console.log(i);
-//   }
-// }
+const totalSum = 1 + 2;
 
-// console.time("myTimer");
-// counter();
-// console.timeEnd("myTimer");
+console.log("my " + "name " + "is " + totalSum + " Hesham");
 
-// console.time("myTimer");
-// counter();
-// console.timeEnd("myTimer");
+console.log(`my name is Hesham ${totalSum}`);
 
-// console.time("myTimer");
-// counter();
-// console.timeEnd("myTimer");
+// console.clear();
+// console.table();
 
-// function sum(num1, num2) {
-//   return num1 + num2;
-// }
+console.log(responseDataOne);
+console.table(responseDataOne);
 
-// let number1 = 10;
-// console.log(number1);
+//
+// let city = "Cairo";
+//
+// console.log("City: ", city);
+//
+// city = "Oslo";
+// console.log("City: ", city);
+//
 
-// let number2 = 20;
-// console.log(number2);
+console.time(); // This starts the timer
+console.timeEnd(); // This ends the timer
 
-// let total = sum(number1, number2);
-// console.log("total:", total);
+function counter() {
+  for (let i = 1; i <= 3; i++) {
+    console.log(i);
+  }
+}
 
-// function getAverage(num1, num2, num3) {
-//   return (num1 + num2 + num3) / 3;
-// }
+console.time("myTimer");
+counter();
+console.timeEnd("myTimer");
 
-// let myAverege = getAverage(2, 5, 5);
-// console.log("myAverege", myAverege);
+console.time("myTimer");
+counter();
+console.timeEnd("myTimer");
 
-// const myArray = [1, 3, 4, 5];
+console.time("myTimer");
+counter();
+console.timeEnd("myTimer");
 
-// function getArrAverege(arr) {
-//   var total = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     total += arr[i];
-//   }
-//   console.log("total", total);
-//   const numberOfItemsInArr = arr.length;
-//   console.log("numberOfItemsInArr", numberOfItemsInArr);
-//   let average = total / numberOfItemsInArr;
-//   return average;
-// }
+// to the average of these number
 
-// const resultOfAverege = getArrAverege(myArray);
-// console.log("resultOfAverege", resultOfAverege);
+let average = (2 + 4 + 7) / 3;
+console.log(average);
 
-// async function getCattyFacts() {
-//   const response = await fetch("https://catfact.ninja/facts");
-//   console.log(response);
-//   const catFacts = await response.json();
-//   console.log(catFacts);
-//   console.log(catFacts.data);
-//   return catFacts;
-// }
+// 1.
 
-// getCattyFacts();
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+let number1 = 10;
+console.log(number1);
+let number2 = 20;
+console.log(number2);
+
+let total = sum(number1, number2);
+
+console.log("total: ", total);
+
+// Hey could you create a function which calculate the average of
+// given 3 numbers (params)
+// please name this function getAverage()
+
+function getAverage(num1, num2, num3) {
+  return (num1 + num2 + num3) / 3;
+}
+
+let myAverage = getAverage(2, 4, 5);
+console.log("myAverage: ", myAverage);
+
+const myArray = [1, 3, 4, 5];
+//
+// create a function which gets the average of the numbers in this array
+
+function getArrAverage(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  const numberOfItemInArr = arr.length;
+  return total / numberOfItemInArr;
+}
+
+const resultAverage = getArrAverage(myArray);
+
+console.log("resultAverage: ", resultAverage);
+
+// when you make an API call
+//
+// https://catfact.ninja/fact
+
+function getCatFacts() {
+  fetch("https://catfact.ninja/facts")
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonResponse) => {
+      console.log(jsonResponse);
+      console.log(jsonResponse.data);
+      console.log(jsonResponse.links);
+    })
+    .catch((error) => {
+      //TODO handle error
+    });
+}
+
+getCatFacts();
+
+async function getCatyFacts() {
+  const response = await fetch("https://catfact.ninja/facts");
+  console.log("response: ", response);
+  const catFacts = await response.json();
+  console.log(catFacts);
+  console.log(catFacts.data);
+  return catFacts;
+}
+
+getCatyFacts().then((r) => {});
+
+function counterTwo() {
+  for (let i = 1; i <= 3; i++) {
+    console.log(i === 2);
+  }
+}
+
+counterTwo();
